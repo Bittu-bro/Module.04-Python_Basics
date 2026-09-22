521a04d6b067f5d6c06e6fb3166f18aa847332fd
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-23 05:08:34 +0530

    Data Type

---
+++ 04. Primitive Datatypes.py
@@ -0,0 +1,24 @@
+# #we have some data type
+
+# string
+name = "Bittu"
+# integers
+age = 18
+# floats
+height = 5.5
+# boolean
+is_student = True
+# none
+null = None #capital 'N' must be important
+
+
+print(type(name), type(age), type(height), type(is_student), type(null))
+
+
+print(name, "ji")
+print(age)
+print(height)
+print(is_student)
+print("Hello, " + name)
+print(len(name))
+
