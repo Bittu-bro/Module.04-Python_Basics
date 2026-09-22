ea57f4fdf21df2e9d73b3df9c763f4e00d62428b
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-23 00:32:29 +0530

    File Initiate

---
+++ 2. Variables.py
@@ -0,0 +1,20 @@
+a = 1
+print(a)
+b = 3.4
+print(b)
+c = True
+print(c)
+d = "Python"
+print(d)
+e = "My name is Bittu"
+print(e)
+f = "i am learning python"
+print(f)
+print(a + b)
+print(a - b)
+print(a * b)
+print(e + f)
+g = "hello"
+h = "world"
+print(g + h)
+print(g + " " + h)
