75aeb51c9cc385d5394ca8a1e91b42b044aaa947
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-23 05:50:25 +0530

    Type Casting

---
+++ 06. Type conversion - int, float, str.py
@@ -0,0 +1,49 @@
+#  #we have some data type in python to type conversion / type casting
+# age = "18"
+# height = "5.5"
+# student = "True"
+
+# age = int(age)
+# height = float(height)
+# student = bool(student)
+
+# print(age)
+# print(type(age))
+
+# print(height)
+# print(type(height))
+
+# print(student)
+# print(type(student))
+
+
+
+
+
+
+
+s1 = 100
+s2 = 392.23
+s3 = 'hello bittu'
+s4 = False
+s5 = "python 3.14.7"
+s6 = "212"
+s10 = 1
+
+num1 = int(s1), float(s1), str(s1), bool(s1)
+num2 = int(s2), float(s2), str(s2), bool(s2)
+num3 = str(s3), bool(s3)
+num4 = int(s4), float(s4), str(s4), bool(s4)
+num5 = str(s5), bool(s5)
+num6 = int(s10), float(s10), str(s10), bool(s10)
+
+print(type(s1)) 
+print(type(s2))
+print(type(s3))
+print(type(s4))
+
+print(num5)
+print(num4)
+print(num2)
+
+print((s3) + " " + str(s1))
