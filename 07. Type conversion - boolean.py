3cb36299785af29f9ef407660aa5f8d010e27c4e
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-23 05:52:36 +0530

    Complete Type Casting

---
+++ 07. Type conversion - boolean.py
@@ -0,0 +1,28 @@
+s4 = False
+s5 = "python 3.14.7"
+s6 = "212"
+s7 = "322"
+s8 = int(s6)+int(s7)
+s9 = "23923.2323"
+s10 = 1
+s11 = None
+
+
+
+num7 = bool(int(s10)), bool(str(s10)), bool(str(s10)), bool(bool(s10))
+num8 = bool(s11), bool(str(s11)), bool(str(s11)), bool(bool(s11))
+num9 = int(int(s4)), str(str(s4)), int(float(s4)), int(bool(s4))
+num10 = float(int(s4)), str(str(s4)), float(float(s4)), float(bool(s4))
+
+
+
+
+print(s6 + s7)
+print(float(s6)+float(s7))
+print(float(s8))
+print(float(s9))
+print(num7)
+print(num8)
+print(num9)
+print(num10)
+print(s11)
