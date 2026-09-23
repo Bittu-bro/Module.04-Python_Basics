f10ef6bebdd25ccefa02e80e1e7b14837a343fb3
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-23 05:37:14 +0530

    Basics Opration

---
+++ 05. Basic operations with strings.py
@@ -0,0 +1,50 @@
+# s1 = 'hello world'
+# s2 = "i am learning python "
+# s3 = '''python is fun
+# I am enjoying Python'''
+# s4 = """i am learnig 5th topic of python
+# what's the your position"""
+# print(s1, type(s1))
+# print(s2, type(s2))
+# print(s3, type(s3))
+# print(s4, type(s4))
+
+#these all are same nothing change in their property
+
+
+#
+# eg 01
+s1 = 'hello world'
+s2 = "i am learning python"
+s3 = """hey how are you.
+do you remember ,
+whene we are meeting last time
+bye."""
+
+# print(s1, type(s1))
+# print(s2, type(s2))
+# print(s3, type(s3))
+
+
+
+
+"""
+ 0    1.   2    3.    4.    5    6.     7.    8    9 #positive indexing
+-10  -9   -8   -7    -6    -5    -4    -3    -2   -1 #negative indexing
+"""
+
+#lenght of the string 
+print(len(s2))
+print(len(s1))
+print(len(s3))
+
+
+#indexing
+print(s1[0])
+print(s2[-1])
+
+
+#++addition++
+
+print(s1 + s2)
+print(s1 + " " +s2)
