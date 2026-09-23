9ca951f1aaf11f93bb76c6638304033fc877280b
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-23 13:08:14 +0530

    All oprater

---
+++ 08. Arithmetic & Assignment Operators.py
@@ -0,0 +1,47 @@
+num1 = 12
+num2 = 2
+
+#addition
+a = num1 + num2
+
+
+#subtraction
+b = num1 - num2
+
+
+#multiplication
+c = num1 * num2
+
+
+#division
+d = num1 / num2
+
+
+#floor division
+e = num1 // num2
+
+
+#models oprater
+f = num1 % num2
+
+
+#Exponent(star star oprater)
+g = num1 ** num2
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+## assignment oprater
+num1 += 5
+print(num1)
+num1 -= 10
+print(num1)
+num1 *= 4
+print(num1)
+num1 /= 11
+print(num1)
