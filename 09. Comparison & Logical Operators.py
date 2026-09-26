0650c4f22f539a06a8440e166a8f0a71d9773206
Bittu-bro <crea.sarcastics@gmail.com>
2026-09-26 23:39:47 +0530

    Comparison Operators

---
+++ 09. Comparison & Logical Operators.py
@@ -0,0 +1,60 @@
+# #Comparison Operators 
+# num1 = 4567
+# num2 = 4053
+# #num1 = "Python"
+# #num2 = "Python"
+
+ 
+# fun1 = num1 == num2
+# fun2 = num1 != num2
+# fun3 = num1 <  num2
+# fun4 = num1 >  num2
+# fun5 = num1 <= num2
+# fun6 = num1 >= num2
+
+
+# print(fun1)
+# print(fun2)
+# print(fun3)
+# print(fun4)
+# print(fun5)
+# print(fun6)
+
+
+# Logical operators
+
+# # and (if any one statement is False we got "Fasle" as result)
+# condition1 = False and True
+# condition2 = True and True
+# print(condition1)
+# print(condition2)
+
+
+# # or (if any one statement is True we got "True" as result)
+# condition3 = False or True
+# condition4 = True or True
+# condition5 = False or False
+
+# print(condition3)
+# print(condition4)
+# print(condition5)
+
+
+# # not (if we are enter "not True", we got as result "Fasle".)
+# # (if we are enter "not False", we got as result "True".)
+# condition6 = not False 
+# condition7 = not True
+
+# print(condition6)
+# print(condition7)
+
+
+## Logical operators(Applicaton)
+name = 'Bittu'
+age = 20
+
+con1 = (name == 'Bittu' and age == 18)
+con2 = (name == 'Bittu' or age == 18)
+
+print(con1)
+print(con2)
